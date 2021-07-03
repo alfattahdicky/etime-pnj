0acc8a9959796454ba339a2addf7a1fe20a3d355
alfattahdicky <alfattahdicky2@gmail.com>
2021-07-03 21:03:00 +0700

    update styling & typograph

--- src/script/home.js
+++ src/script/home.js
@@ -1,26 +1,19 @@
 // Control Video
 const videoEl = document.getElementById('video');
 const close = document.getElementById('close');
-const brandOmron = document.getElementById('video-omron');
 const brandAntares = document.getElementById('video-antares');
 function closeIklan() {
   close.style.display = 'none';
   videoEl.style.display = 'none';
 }
 
-const stopBrand = () => {
-  brandOmron.classList.add('d-none');
-  brandOmron.currentSrc = '';
-}
-
 const stopBrandAntares = () => {
   brandAntares.classList.add('d-none');
   brandAntares.currentSrc = '';
 }
-const dateEnd = () => new Date().getDate() === 2;
+const dateEnd = () => new Date().getDate() === 4;
 
-setTimeout(() => stopBrand(), 48000)
 setTimeout(() => stopBrandAntares(), 32000)
 
-if (dateEnd()) stopBrand();
+if (dateEnd()) stopBrandAntares();
 close.addEventListener('click', closeIklan);
