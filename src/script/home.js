1f2daeb5d6a6491a8946f3e8915873a42a79585e
alfattahdicky <alfattahdicky2@gmail.com>
2021-07-05 22:00:03 +0700

    update speaker

--- src/script/home.js
+++ src/script/home.js
@@ -1,19 +1,1 @@
-// Control Video
-const videoEl = document.getElementById('video');
-const close = document.getElementById('close');
-const brandAntares = document.getElementById('video-antares');
-function closeIklan() {
-  close.style.display = 'none';
-  videoEl.style.display = 'none';
-}
 
-const stopBrandAntares = () => {
-  brandAntares.classList.add('d-none');
-  brandAntares.currentSrc = '';
-}
-const dateEnd = () => new Date().getDate() === 4;
-
-setTimeout(() => stopBrandAntares(), 32000)
-
-if (dateEnd()) stopBrandAntares();
-close.addEventListener('click', closeIklan);
